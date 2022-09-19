c48c59283b3d2641b7aea46289486d11903262a9
Vadim Gamzaev <Vadosbox@gmail.com>
2022-09-19 16:54:19 +0300

    Initial Commit

---
+++ SliderUIKit+SwiftUI/SliderUIKit_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderUIKit_SwiftUIApp.swift
+//  SliderUIKit+SwiftUI
+//
+//  Created by Вадим Гамзаев on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderUIKit_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
